9e42947f8861757e72492950883c2a2f6ea32627
forger6969 <saidazim186@gmail.com>
2026-04-01 01:15:08 +0500

    asd

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -5,7 +5,7 @@ const userSchema = mongoose.Schema({
     github_id:{type:Number , required:true},
     github_token:{type:String , required:true},
     avatar_url:{type:String},
-    email:{type:String , required:true},
+    email:{type:String , },
     name:{type:String},
     bio:{type:String},
      refresh_tokens: [{
