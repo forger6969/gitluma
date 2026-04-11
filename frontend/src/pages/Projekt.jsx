9051ff5569bac29ec69975fc672225b490130f56
forger6969 <saidazim186@gmail.com>
2026-04-11 15:54:08 +0500

    add project.js add getmy projects

--- frontend/src/pages/Projekt.jsx
+++ frontend/src/pages/Projekt.jsx
@@ -1,11 +1,19 @@
 import React, { useEffect } from 'react'
+import { useSelector } from 'react-redux'
 const Projekt = () => {
+
+const projects = useSelector((state)=> state.projects.projects)
+const loading = useSelector((state)=> state.projects.loading)
+
+useEffect(()=>{
+console.log(projects);
+
+},[projects])
 
     return (
         <div className='flex-1 h-[91%] bg-gray-950 flex justify-center items-center '>
 
-            Projektssssslkasdlasjd
-
+            laskjdlaksd
         </div>
     )
 }
